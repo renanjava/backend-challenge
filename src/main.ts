18e43e48b459189a8673445ecea8493db4e2df1a
Renan <renancvr777@gmail.com>
2025-05-01 03:12:14 -0300

    feat: biblioteca dotenv para lidar com variáveis de ambiente

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { Logger, NotFoundException } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT ?? 3000)
+
+  const configService = app.get(ConfigService)
+
+  const applicationPort = configService.get<number>('PORT')
+  if (!applicationPort) {
+    throw new NotFoundException('Variável de ambiente PORT não definida')
+  }
+
+  await app.listen(applicationPort)
+  Logger.log(`Server is running on port ${applicationPort}`, 'ApplicationPort')
 }
 void bootstrap()
